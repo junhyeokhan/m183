ac95627aa0a1d2fe5cbcfca4f4021157124cf688
junhyeokhan <junhyeokhan.it@gmail.com>
2017-09-29 09:25:56 +0200

    SSO - FrontEnd is finish

---
+++ SingleSignOn/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingleSignOn.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public JsonResult SSOTokenSignIn()
+        {
+            bool isVerified = false;
+            //Verify Token
+
+            if (isVerified)
+            {
+                //
+            }
+
+            return Json("");
+        }
+    }
+}
