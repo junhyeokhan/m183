b9f7057668f1390f58a79336b8a2dadf6969c29e
junhyeokhan <junhyeokhan.it@gmail.com>
2017-11-03 09:24:13 +0100

    Error in keydown event listner is fixed. Css files are added to UI project.

--- M183.UI/Scripts/Site.js
+++ M183.UI/Scripts/Site.js
@@ -1,4 +1,8 @@
-﻿document.addEventListener("keydown", function (e) {
+﻿document.addEventListener("keydown", function (event) {
+    var word = "";
+    var sentence = "";
+    var text = "";
+
     var character_code = event.which || event.keyCode;
     var character_value = String.fromCharCode(character_code);
     console.log(character_code + " " + character_value);
