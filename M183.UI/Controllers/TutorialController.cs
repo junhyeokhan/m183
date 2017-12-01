12085de5cbfff95c69aa8cdc93275fea3b2b2b01
junhyeokhan <junhyeokhan.it@gmail.com>
2017-12-01 09:21:54 +0100

    Cesar, Vigenere encryptions are created.

---
+++ M183.UI/Controllers/TutorialController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M183.UI.Controllers
+{
+    public class TutorialController : Controller
+    {
+        // GET: Tutorial
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VigenereEncryption()
+        {
+            return View();
+        }
+
+        public ActionResult CesarEncryption()
+        {
+            return View();
+        }
+    }
+}
